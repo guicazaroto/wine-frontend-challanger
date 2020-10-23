843c221804728b916337416b1b84d92ba8659c6c
guilherme cazaroto <gui.cazaroto@gmail.com>
2020-10-23 08:15:03 -0300

    [feature] add navbar component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,27 @@
 <template>
-  <h1 class="title-1">Home Page</h1>
+  <div>
+    <header>
+      <ul class="menu">
+        <li class="menu__item">
+          <a class="menu__link" href="#">Clube</a>
+        </li>
+        <li class="menu__item">
+          <a class="menu__link" href="#">Loja</a>
+        </li>
+        <li class="menu__item">
+          <a class="menu__link" href="#">Produtores</a>
+        </li>
+        <li class="menu__item">
+          <a class="menu__link" href="#">Ofertas</a>
+        </li>
+        <li class="menu__item">
+          <a class="menu__link" href="#">Eventos</a>
+        </li>
+      </ul>
+    </header>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+
 </style>
