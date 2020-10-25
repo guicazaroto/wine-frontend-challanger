17313ec4112af24c39ac4852dbfc201d6b31c316
guilherme cazaroto <gui.cazaroto@gmail.com>
2020-10-25 11:50:06 -0300

    [feature] add to localstorage

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -9,14 +9,15 @@
           <h3 class="product__description">{{ product.name }}</h3>
           <div class="buy-box">
             <div v-if="product.available" class="price-info">
-              <span class="product__price">R$ {{ product.priceStock }}</span>
+              <span class="product__price">R$ {{ formatPrice(product.priceStock) }}</span>
               <span class="product__customer">sócio wine</span>
               <p class="product__member-price">
                 <span class="symbol">R$</span>
-                <span class="integer">{{ product.priceMember }}</span>
+                <span class="integer">{{ formatPrice(product.priceMember) }}</span>
               </p>
             </div>
             <button
+              @click="addProduct(product)"
               :class="[
                 'btn product__btn desktop',
                 {
@@ -29,12 +30,13 @@
         </div>
       </div>
       <button
-      :class="[
-        'btn product__btn mobile',
-        {
-          'btn--purple': product.available,
-        }]">
-        <span v-if="product.available">Adicionar</span>
+        @click="addProduct(product)"
+        :class="[
+          'btn product__btn mobile',
+          {
+            'btn--purple': product.available,
+          }]">
+          <span v-if="product.available">Adicionar</span>
         <span v-else>Esgotado</span>
       </button>
     </div>
@@ -52,7 +54,55 @@ export default {
     })
   },
   methods: {
-    ...mapActions('products', ['getProducts'])
+    ...mapActions('products', ['getProducts']),
+    formatPrice (price) {
+      return price.toLocaleString('pt-BR', {
+        minimumFractionDigits: 2,  
+        maximumFractionDigits: 2
+      })
+    },
+    addProduct (product) {
+      if (!product.available) return;
+
+      // if has no one product then create storage
+      const data = this.getStorage()
+      if (!data) {
+        this.createNewStorage(product)
+        return
+      }
+
+      // if has no one of current product then create this item
+      const productIndex = data.findIndex(x => x.name === product.name)
+      if (productIndex === -1) {
+        this.storeNewProduct(product, data)
+        return
+      }
+
+      // if already has an product, add quantity
+      this.storeExistingProduct(product, data, productIndex)
+    },
+    getStorage () {
+      const localData = localStorage.getItem('products')
+      const data = JSON.parse(localData)
+      return data
+    },
+    createNewStorage (product) {
+      const store = []
+      store.push({ name: product.name, qtd: 1 })
+      
+      this.setToLocalStorage(store)
+    },
+    storeExistingProduct (product, data, index) {
+      data[index].qtd++
+      this.setToLocalStorage(data)
+    },
+    storeNewProduct (product, data) {
+      data.push({ name: product.name, qtd: 1 })
+      this.setToLocalStorage(data)
+    },
+    setToLocalStorage (data) {
+      localStorage.setItem('products', JSON.stringify(data))
+    }
   },
   created () {
     this.getProducts()
