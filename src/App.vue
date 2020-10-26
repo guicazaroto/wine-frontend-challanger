4c5f63841226947d74212495549fab0d48cf4ce2
guilherme cazaroto <gui.cazaroto@gmail.com>
2020-10-26 13:42:05 -0300

    [feature] SEO Prerender

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
-<template>
-  <router-view />
+<template>  
+  <div id="app">
+    <router-view />
+  </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,17 @@ import Banner from '@/components/Banner'
 import Products from '@/components/Products'
 
 export default {
+  name: 'Home',
+  metaInfo: {
+  htmlAttrs: {
+    lang: 'pt-BR'
+  },
+  title: 'Wine | Compre vinhos online com descontos exclusivos!',
+  meta: [{
+      name: 'descrition',
+      content: 'Vinho tinto, rosé, branco ou espumante. Na Wine, você encontra os melhores rótulos pelos menores preços. Acesse a loja ou baixe nosso aplicativo!'
+    }],
+  },
   components: {
     Banner,
     Products
