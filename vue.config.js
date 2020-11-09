4b4aff7865c6747e3767ceeb5ed4871843dc0a1c
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2020-11-09 19:24:30 -0300

    [feature] headless

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
 module.exports = {
   configureWebpack: {
@@ -7,6 +8,9 @@ module.exports = {
       new PrerenderSPAPlugin({
         staticDir: path.join(__dirname, 'dist'),
         routes: ['/'],
+        renderer: new Renderer({
+          headless: false
+        })  
       }),
     ],
   },
